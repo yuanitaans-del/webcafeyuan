3a921cd1394d5a32ac4c9eea3b1825be368ff82a
yuanitaans-del <yuanitaa.ns@gmail.com>
2026-05-24 21:09:50 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,6 +196,10 @@ $(function() {
     
 });
 
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+$(document).ready(function () {
+    $(".navbar-nav a").on("click", function () {
+        $(".navbar-collapse").removeClass("show");
+        $(".navbar-toggler").addClass("collapsed");
+        $(".navbar-toggler").attr("aria-expanded", "false");
+    });
 });
